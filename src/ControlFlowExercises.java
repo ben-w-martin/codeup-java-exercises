5fd12667fba2fb9595a7d2ff92d51589f631a17f
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-13 17:30:43 -0500

    while and do while

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,21 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+// todo Loop Basics*****************************************************************
+        /* todo While-----------------------------------------------------------------------
+         *      int i = 5;
+         *
+         *      while (i <= 15) {
+         *      System.out.println(i);
+         *      i++;
+         *      }
+         */
+
+// todo Do While--------------------------------------------------------------------
+    // Count by twos
+        int i = 0;
+        do {
+            i +=2;
+            System.out.println(i);
+        } while (i < 100);
+    }
+}
