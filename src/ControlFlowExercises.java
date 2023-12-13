55958ee02a31c1ed14af025fa89478f0a865e779
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-13 18:56:27 -0500

    Number table complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 // todo Loop Basics*****************************************************************
@@ -36,7 +38,8 @@ public class ControlFlowExercises {
 //
 //        } while (i < 1_000_000);
 
-//        For Loops
+// todo For Loops ------------------------------------------------------------------
+
 //        fives
 //        for (int i = 100; i >= -10; i -= 5) {
 //            System.out.println(i);
@@ -47,6 +50,39 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+//        FizzBuzz
+//        for (int i = 0; i <= 100; i++) {
+//            if ((i % 3 == 0) && (i % 5 != 0) ) {
+//                System.out.println("Fizz");
+//            } else if ((i % 3 != 0) && (i % 5 == 0)) {
+//                System.out.println("Buzz");
+//            } else if ((i % 3 == 0) && (i % 5 == 0) && (i != 0)) {
+//                System.out.println("FizzBuzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+//        Table of Powers
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an int: ");
+        int userInt = sc.nextInt();
+
+        System.out.printf("Here is your table based on the input: --> %d <--\n\n", userInt);
+
+        System.out.printf("number | squared | cubed\n");
+        System.out.printf("------ | ------- | -----\n");
+
+        int i = 1;
+
+        while (i <= userInt) {
+            int squared = i;
+            int cubed = i;
+            squared = (int) Math.pow(squared, 2);
+            cubed = (int) Math.pow(cubed, 3);
+            System.out.printf(" %d     | %d       | %d   \n", i, squared, cubed);
+            i++;
+        }
 
     }
 }
