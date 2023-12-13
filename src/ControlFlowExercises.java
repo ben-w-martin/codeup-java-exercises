582086cf6951b110deccbfa48277be922dc87854
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-13 17:53:33 -0500

    exponents of two

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,29 +1,40 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 // todo Loop Basics*****************************************************************
-        /* todo While-----------------------------------------------------------------------
-         *      int i = 5;
-         *
-         *      while (i <= 15) {
-         *      System.out.println(i);
-         *      i++;
-         *      }
-         */
+// todo While ----------------------------------------------------------------------
+
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
 
 // todo Do While--------------------------------------------------------------------
-// Count by twos
+
+//        Count by twos
 //        int i = 0;
 //        do {
-//            i +=2;
+//            i += 2;
 //            System.out.println(i);
 //        } while (i < 100);
 
 
-// Count down by fives
-        int i = 105;
+//        Count down by fives
+//        int i = 105;
+//        do {
+//            i -= 5;
+//            System.out.println(i);
+//        } while (i > -10);
+
+//        Twos squared
+        int i = 2;
         do {
-            i -= 5;
             System.out.println(i);
-        } while (i > -10);
+            i = (int) Math.pow(i, 2);
+
+        } while (i < 1_000_000);
+
     }
 }
