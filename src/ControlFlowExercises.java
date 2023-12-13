8a035860971894b7242e0df0f31f0bbdb3ec24c2
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-13 17:33:40 -0500

    count by 5's backwards

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -11,11 +11,19 @@ public class ControlFlowExercises {
          */
 
 // todo Do While--------------------------------------------------------------------
-    // Count by twos
-        int i = 0;
+// Count by twos
+//        int i = 0;
+//        do {
+//            i +=2;
+//            System.out.println(i);
+//        } while (i < 100);
+
+
+// Count down by fives
+        int i = 105;
         do {
-            i +=2;
+            i -= 5;
             System.out.println(i);
-        } while (i < 100);
+        } while (i > -10);
     }
 }
