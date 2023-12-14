d76bd26cb1e417627c20572ac8df060cb659fcbf
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-14 16:33:04 -0500

    How can you have any pudding if you don't eat your meat?

--- src/StringsLecture.java
+++ src/StringsLecture.java
@@ -1,2 +1,8 @@
 public class StringsLecture {
+    public static void main(String[] args) {
+        System.out.println("con" + "cat");
+        String con = "con";
+        System.out.println(con.concat("cat"));
+        System.out.println(con);
+    }
 }
