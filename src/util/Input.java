4dbada7635b917faa43da5e3153f362ea961b261
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-18 17:15:48 -0500

    current: working on getDouble

--- src/util/Input.java
+++ src/util/Input.java
@@ -8,15 +8,22 @@ public class Input {
     private int userMin;
     private int userMax;
     private int userInt;
+    private double userDoubleMin;
+    private double userDoubleMax;
+    private double userDouble;
 
 
     public Input() {
         System.out.println("Type a string, a min, a max, and a number in between.");
+        System.out.println("Type three decimal numbers, a min, a max, and a number in between.");
         Scanner scanner = new Scanner(System.in);
         userInput = scanner.next();
         userMin = scanner.nextInt();
         userMax = scanner.nextInt();
         userInt = scanner.nextInt();
+        userDoubleMin = scanner.nextInt();
+        userDoubleMax = scanner.nextInt();
+        userDouble = scanner.nextDouble();
     }
 
     public String getString() {
@@ -25,6 +32,10 @@ public class Input {
 
     public boolean yesNo() {
         return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
+    }
+
+    public int getInt() {
+        return userInt;
     }
 
     public int getInt(int min, int max) {
@@ -38,11 +49,23 @@ public class Input {
         }
     }
 
-    public int getInt() {
-        return userInt;
+    public double getDouble(double min, double max) {
+
+        if (userDouble >= min && userDouble <= max) {
+            System.out.printf("%f is within min: %f, and max %f.", userDouble, min, max);
+            return userDouble;
+        } else {
+            System.out.printf("%f is NOT within min: %f, and max %f.", userDouble, min, max);
+            return userDouble;
+        }
     }
 
-//        String getString() **
+    public double getDouble() {
+        return userDouble;
+    }
+
+
+//        String getString()
 //        boolean yesNo()
 //        int getInt(int min, int max)
 //        int getInt()
@@ -55,6 +78,7 @@ public class Input {
         System.out.println(newInput.yesNo());
         System.out.println(newInput.getInt());
         System.out.println(newInput.getInt(newInput.userMin, newInput.userMax));
+        System.out.println(newInput.getDouble(newInput.userMin, newInput.userMax));
     }
 
 }
